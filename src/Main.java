1ff47d4d84c8ffb9648d16fcfa2e02d1e44df42e
Lena <l.zubtsova@mail.ru>
2023-03-20 21:31:08 +0300

    zubtsova

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
-        Zubtsova zubtsova = new Zubtsova();
-        System.out.println(zubtsova);
+
 
 
     }
